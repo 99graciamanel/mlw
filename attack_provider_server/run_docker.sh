739e08bf5f3f03c946f52f0722e4b194ceb83909
Manel <manel.gracia.sola@gmail.com>
2021-12-02 16:24:58 +0100

    Docker in daemon mode

--- attack_provider_server/run_docker.sh
+++ attack_provider_server/run_docker.sh
@@ -3,4 +3,4 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker build -t attack-provider-image .
-docker run -p 80:8080 -t attack-provider-image . -d
+docker run -d -p 80:8080 -t attack-provider-image .
